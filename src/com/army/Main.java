eec97727083fcb0218ea085955b4f0f1fa431206
20fut <231612@astanait.edu.kz>
2024-02-25 23:38:08 +0600

    commit with new class names

--- src/com/army/Main.java
+++ src/com/army/Main.java
@@ -2,12 +2,15 @@ package com.army;
 
 import com.army.controllers.Medical_info_controller;
 import com.army.controllers.Personal_info_controller;
+import com.army.controllers.Registration_info_controller;
 import com.army.database.postgresql_db;
 import com.army.database.postgresql_idb;
 import com.army.repositories.Medical_info_Repository;
 import com.army.repositories.Personal_info_Repository;
+import com.army.repositories.Registration_info_repository;
 import com.army.repositories.interfaces.IMedical_info_Repository;
 import com.army.repositories.interfaces.IPersonal_info_repository;
+import com.army.repositories.interfaces.IRegistration_info_repository;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,7 +19,9 @@ public class Main {
         Personal_info_controller pcontroller = new Personal_info_controller(prep);
         IMedical_info_Repository mrep =new Medical_info_Repository(db);
         Medical_info_controller mcontroller = new Medical_info_controller(mrep);
-        MainFunctions app = new MainFunctions(pcontroller,mcontroller);
+        IRegistration_info_repository rrep = new Registration_info_repository(db);
+        Registration_info_controller rcontroller = new Registration_info_controller(rrep);
+        MainFunctions app = new MainFunctions(pcontroller,mcontroller,rcontroller);
         app.start();
     }
 }
