f30e3f11b9a87c4b717fb06d7574619a0ad942df
dvn1vl <154106380+dvn1vl@users.noreply.github.com>
2024-02-26 01:34:38 +0600

    Adding Registration_info

--- src/com/army/controllers/Registration_info_controller.java
+++ src/com/army/controllers/Registration_info_controller.java
@@ -1,4 +1,55 @@
 package com.army.controllers;
 
+import com.army.models.Personal_info;
+import com.army.models.Registration_info;
+import com.army.repositories.interfaces.IPersonal_info_repository;
+import com.army.repositories.interfaces.IRegistration_info_repository;
+
+import java.util.List;
+
 public class Registration_info_controller {
+    private final IRegistration_info_repository rep;
+
+    public Registration_info_controller(IRegistration_info_repository rep) {
+        this.rep = rep;
+    }
+    public String to_register(String phone_number,String password) {
+        Registration_info user = new Registration_info(phone_number,password);
+        boolean created = rep.to_register(user);
+        return (created ? "Successfully registrated!" : "Registration process was failed!");
+    }
+    public String getRegistration_info(int id) {
+        Registration_info user = rep.getRegistration(id);
+        return (user == null ? "User was not found!" : user.toString());
+    }
+    public boolean isRegistationID(int id) {
+        Registration_info user = rep.getRegistration(id);
+        return (user== null ? false : true);
+    }
+    public boolean isRegistationPhone_number(String phone) {
+        Registration_info user = rep.getRegistrationByPhone(phone);
+        return (user== null ? false : true);
+    }
+    public int ShowId(String phone){
+        Registration_info user = rep.getRegistrationByPhone(phone);
+        return user.getId();
+    }
+    public String getAllRegistrations() {
+        List<Registration_info> users = rep.getAllRegistrations();
+
+        StringBuilder answer = new StringBuilder();
+        for (Registration_info user : users) {
+            answer.append(user.toString()).append("\n");
+        }
+        return answer.toString();
+    }
+    public boolean Log_in(String phone, String password){
+        Registration_info user = new Registration_info(phone,password);
+        boolean logged_in = rep.Log_in(user);
+        return logged_in;
+    }
+    public boolean check_to_admin(String phone, String password){
+        Registration_info user = new Registration_info(phone,password);
+        return user.isAdmin();
+    }
 }
--- src/com/army/models/Registration_info.java
+++ src/com/army/models/Registration_info.java
@@ -1,4 +1,59 @@
 package com.army.models;
 
-public class Registration_info {
+public class Registration_info extends ID {
+    private String phone_number;
+    private String password;
+    private boolean admin;
+
+    public Registration_info() {
+    }
+
+    public Registration_info(String phone_number, String password) {
+        this.phone_number = phone_number;
+        this.password = password;
+        this.admin = isDefaultAdmin(phone_number, password);
+    }
+
+    public Registration_info(int id, String phone_number, String password, boolean admin) {
+        super(id);
+        this.phone_number = phone_number;
+        this.password = password;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "Registration_info{" +
+                "phone_number='" + phone_number + '\'' +
+                ", password='" + password + '\'' +
+                ", admin=" + admin +
+                '}';
+    }
+
+    public boolean isDefaultAdmin(String phone_number, String password) {
+        return phone_number.equals("7077405280") && password.equals("did12321");
+    }
 }
